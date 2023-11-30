84502d2d8a80506c7f712b1fc62188aefba9d72a
Xitlali Reyes <xitlalir59@gmil.com>
2023-11-30 03:24:57 -0600

    BD
    BD con algunos productos de prueba integrados (para importar)

---
+++ sirenegaze.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-11-2023 a las 09:51:43
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sirenegaze`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `Id_producto` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `cantidad` int(10) NOT NULL,
+  `precio` float NOT NULL,
+  `imagen` varchar(50) NOT NULL,
+  `descuento` int(10) NOT NULL,
+  `categoria` varchar(50) NOT NULL,
+  `subcategoria` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`Id_producto`, `nombre`, `descripcion`, `cantidad`, `precio`, `imagen`, `descuento`, `categoria`, `subcategoria`) VALUES
+(1, 'Vestido mini bandeau tablas rayas', 'composición\r\n74% viscosa\r\n23% poliamida\r\n3% elastano', 50, 899, 'woman_vestido.jpg', 0, 'woman', 'vestidos'),
+(2, 'Jersey cuello redondo rayas', 'Composición\r\n53% poliéster\r\n28% acrílico\r\n16% poliamida\r\n3% lana', 50, 699, 'woman_sueter2.jpg', 10, 'woman', 'sueteres'),
+(3, 'Cazadora dad fit efecto piel', 'Recubrimiento\r\n100% poliuretano\r\nTejido base\r\n100% poliéster\r\nForro\r\n100% poliéster', 50, 899, 'woman_chamarra.jpg', 5, 'woman', 'chamarras'),
+(4, 'Camisa manga corta jacquard calados', 'Composición\r\n61% poliéster\r\n33% algodón\r\n6% viscosa', 20, 699, 'man_camisa_mc.jpg', 5, 'men', 'camisas'),
+(5, 'Sudadera capucha', 'Composición\r\nSecundario\r\n97% algodón\r\n3% elastano\r\nPrincipal\r\n70% algodón\r\n30% poliéster\r\nForro\r\n70%', 80, 499, 'man_sudadera.jpg', 50, 'men', 'sudaderas'),
+(6, 'Jeans baggy cargo tiras', 'Composición\r\nExterior\r\n100% algodón', 10, 1299, 'woman_pantalon_baggy_cargo1.jpg', 40, 'woman', 'pantalones'),
+(7, 'Camiseta manga larga combinada terciopelo', 'Composición\r\n92% poliéster\r\n8% elastano\r\nPrincipal\r\n90% poliéster\r\n10% elastano', 4, 599, 'woman_blusa.jpg', 15, 'woman', 'blusas'),
+(8, 'Camiseta tirantes rib', 'Composición\r\n95% algodón\r\n5% elastano', 90, 299, 'man_camisa1.jpg', 60, 'men', 'camisas');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`Id_producto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
