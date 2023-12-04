e5c1d2c397c2a8ccccebdb3904563a07bcedc01f
Xitlali Reyes <xitlalir59@gmil.com>
2023-12-03 19:51:33 -0600

    Footer
    Implementación del footer

---
+++ footer.php
@@ -0,0 +1,39 @@
+<link rel="stylesheet" href="css/footer2.css">
+
+<footer class="pie-pagina" id="footer">
+        <div class="grupo-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="imagenes/Logo_sf.png" alt="Logo">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>Ubicación</h2>
+                <p><i class="fa-solid fa-map-pin" style="color: white;"></i> Avenida Universidad 940</p>
+                <p><i class="fa-solid fa-splotch" style="color: white; font-size:13px;"></i> Style speaks louder than words.</p>
+                <h2></h2>
+                <p></p>
+            </div>
+            <div class="box">
+                <h2>S i g u e n o s</h2>
+                <div class="red-social">
+                    <a href="#" class="facebook fa fa-facebook "></a>
+                    <a href="#" class="fa fa-instagram instagram"></a>
+                    <a href="#" class="fa fa-twitter twiter"></a>
+                    <a href="#" class="fa fa-youtube youtube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2023 <b>Sirenegaze</b> - Todos los Derechos Reservados.</small>
+        </div>
+    </footer>
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script> -->
+<script src="https://kit.fontawesome.com/673553f744.js" crossorigin="anonymous"></script>
+    <!-- <script src="js/script.js"></script> -->
+
