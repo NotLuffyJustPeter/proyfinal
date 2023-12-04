8a99859b6755c3cf578ef17c48058dfb4e2a0db4
Xitlali Reyes <xitlalir59@gmil.com>
2023-12-03 19:49:47 -0600

    Pagina de inicio
    Actualización del diseño de la pagina de inicio

--- inicio.php
+++ inicio.php
@@ -6,19 +6,22 @@
     <link rel="stylesheet" href="css/sty.css">
     <style>
     .bloque2{
+        /* max-width: 2500px; */
         display: grid;
         grid-template-columns: repeat(3, 1fr);
-        grid-gap: 20px;
-        background-color: rgba(0, 0, 0, 0.078);
-        margin: 100px;
+        grid-gap:0px;
+        /* background-color: rgba(0, 0, 0, 0.078); */
+        background-color: black;
+        margin: 50px;
     }
     .uno{
-        padding: 20px;
         grid-column: span 1; /* El primer elemento ocupa una columna */
     }
-
     .dos{
-        grid-column: span 2; /* El segundo elemento ocupa dos columnas */
+        grid-column: span 1; /* El segundo elemento ocupa dos columnas */
+    }
+    .tres{
+        grid-column: span 1; /* El segundo elemento ocupa dos columnas */
     }
     </style>
 </head>
@@ -28,7 +31,7 @@
             <h1 class="t1">F A S H I O N</h1>
             <h1 class="t2">N E V E R</h1>
             <h1 class="t2" style="margin-left: 180px;">S L E E P S</h1>
-            <i style="margin-left: 500px; margin-top: 0px;" class="fa-solid fa-splotch" style="color: #000000;"></i>
+            <i class="fa-solid fa-splotch fa-flip" style="color: black; margin-left: 500px; margin-top: 0px;"></i>
             <br>
             <img class="logo2" src="imagenes/Log.png" alt="" class="logo">
 
@@ -37,15 +40,20 @@
         </div>
         <div class="imagenes">
             <img class="b1" src="imagenes/imginicio2.jpg" alt="" class="jess">
-            <img class="b1" src="imagenes/jess5.jpg" alt="" class="jess">
-            <img class="b1" src="imagenes/man_chamarra.jpg" alt="" class="jess">
             <img class="b1" src="imagenes/jess2.jpg" alt="" class="jess">
+            <img class="b1" src="imagenes/imginicio6.jpg" alt="" class="jess">
+            <img class="b1" src="imagenes/imginicio4.jpg" alt="" class="jess">
         </div>
     </div>
 
     <div class="bloque2">
         <div class="uno">
-          <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+          <video width="100%" height="100%" autoplay loop muted>
+            <source src="media/Video_man.mp4" type="video/mp4">
+          </video>
+        </div>
+        <div class="dos">
+          <div id="carouselExampleIndicators" class="carousel slide attachment" data-bs-ride="carousel"> 
             <div class="carousel-indicators">
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
               <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -72,10 +80,14 @@
             </button>
           </div>
         </div>
-        <div class="dos">
-          HOLA MUNDO
+
+        <div class="tres">
+          <video width="100%" height="100%" autoplay loop muted>
+            <source src="media/Video_men.mp4" type="video/mp4">
+          </video>
         </div>
     </div>
+
 
     <h1 class="subtitulo1">W &nbsp&nbspO &nbsp&nbspM &nbsp&nbspA &nbsp&nbspN</h1>
     <div class="bloque4">
@@ -116,9 +128,60 @@
     
 
     <div class="bloque6">
-      <!-- <img class="inicio" src="imagenes/imginicio3.jpg" alt=""> -->
+      <style>
+        .bloque6{
+          display: flex;
+          flex-direction: row;
+          justify-content:center;
+        }
+        .bloque6_uno{
+          width:600px;
+          /* display: flex;
+          flex-direction: row;
+          justify-content:center; */
+        }
+        .tex6{
+          padding-left:40px;
+          font-size:20px;
+          text-align:right;
+        }
+      </style>
+
+        <div class="bloque6_uno">
+          <video width="100%" height="100%" autoplay loop muted>
+            <source src="media/Video_woman.mp4" type="video/mp4">
+          </video>
+        </div>
+
+        <div class="tex6">
+          Fashion is what you buy, style is what you do with it.
+          <h1>Fashion fades, style is eternal.</h1>
+          <pre style="font-size:9px;">
+          Style speaks louder than words. Fashion is my language. Chase the sun in style.
+          Less perfection, more authenticity. Elegance is an attitude.
+          Style is a reflection of your attitude and personality.
+          Confidence is the best accessory. 
+          Clothes aren't going to change the world, the women who wear them will.
+          Style is a way to say who you are without having to speak.
+          Style is knowing who you are, what you want, and not giving a damn.
+          Be your own kind of beautiful.
+          Elegance never goes out of style.
+          Life is too short to wear boring clothes.
+          Dress confidently and conquer the world.
+          Fashion is about making a statement without saying a word. 
+          Fashion is the armor to survive the reality of everyday life.
+          Le style parle plus fort que les mots. 
+          La mode est mon langage. Chasse le soleil avec style.
+          Moins de perfection, plus d'authenticité. L'élégance est une attitude. 
+          Le style est un reflet de ton attitude et de ta personnalité. 
+          La confiance est le meilleur accessoire. 
+          Les vêtements ne vont pas changer le monde, les femmes qui les portent le feront.
+          Le style est une manière de dire qui tu es sans avoir à parler.
+          Le style, c'est de savoir qui tu es, ce que tu veux et de ne pas t'en soucier.
+          Sois ta propre forme de beauté.
+          </pre>
+        </div>
     </div>
 
 </body>
-
 </html>
