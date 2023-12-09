4aeb3111be0fd37b1842fd311cef7211da8e768a
Xitlali Reyes <xitlalir59@gmil.com>
2023-12-09 17:17:31 -0600

    diseño prueba 1 carrito
    diseño carrito prueba 1

--- carrito.php
+++ carrito.php
@@ -10,6 +10,7 @@ if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) > 0) {
     $password = "";
     $dbname = "sirenegaze";
     $tabla = "inventario";
+    $total = 0;
 
     // Crear conexión
     $conn = new mysqli($servername, $username, $password, $dbname);
@@ -27,18 +28,28 @@ if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) > 0) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/carrito.css">
         <title>Carrito de compras</title>
+        <style>
+    
+        </style>
     </head>
     <body>
         <?php include 'header.php'; ?>
         <br><br><br>
-        <h1>Carrito de compras</h1>
         <br><br>
-        <table>
+        <div class="carrito">
+
+        <h1 class="titulo">C a r r i t o &nbsp&nbsp  d e  &nbsp&nbspc o m p r a s</h1>
+        <table class="table table-borderless table-hover prod">
+        
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Imagen</th>
-                    <th>Cantidad</th>
+                    <th class="px-3 can">ID</th>
+                    <th class="px-3 can">Imagen</th>
+                    <th class="px-3 can">Producto</th>
+                    <th class="px-3 can">Descripcion</th>
+                    <th class="px-3 can">Precio</th>
+                    <th class="px-3 can">Cantidad</th>
+                    <th class="px-3 can">Subtotal</th>
                     <!-- Agrega más columnas según sea necesario -->
                 </tr>
             </thead>
@@ -52,17 +63,33 @@ if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) > 0) {
                     if ($result->num_rows > 0) {
                         $row = $result->fetch_assoc();
                         echo '<tr>';
-                        echo '<td>' . $row['Id_producto'] . '</td>';
-                        echo '<td><img src="imagenes/' . $row['imagen'] . '" alt="imagen no cargada"></td>';
-                        echo '<td>' . $row['nombre'] . '</td>';
-                        echo '<td>' . $detallesProducto['cantidad'] . '</td>';
+                        echo '<td class="align-middle px-4 can">' . $row['Id_producto'] . '</td>';
+                        echo '<td class="align-middle px-4"><img class="img_carrito" src="imagenes/' . $row['imagen'] . '" alt="imagen no cargada"></td>';
+                        echo '<td class="align-middle px-4">' . $row['nombre'] . '</td>';
+                        echo '<td class="align-middle px-4">' . $row['descripcion'] . '</td>';
+                        echo '<td class="align-middle px-4 can"> $' . $row['precio'] . '</td>';
+                        echo '<td class="align-middle px-4 can">' . $detallesProducto['cantidad'] . '</td>';
+                        echo '<td class="align-middle px-4 can"> $' . $row['precio'] * $detallesProducto['cantidad'] . '</td>';
+                        echo '<td class="align-middle px-4 can"><button><i class="fa-regular fa-trash-can" style="color: #000000; font-size:25px;"></i></button></td>';
                         // Agrega más columnas según sea necesario
                         echo '</tr>';
                     }
+                    $total = $row['precio'] * $detallesProducto['cantidad'] + $total;
                 }
+                echo '<tr>';
+                    echo '<td colspan="6" style="text-align:center;">T&nbsp O &nbspT&nbsp A&nbsp L</td>';
+                    // echo '<td></td>';
+                    // echo '<td></td>';
+                    // echo '<td></td>';
+                    // echo '<td></td>';
+                    // echo '<td></td>';
+                    echo '<td class="can">$' . $total .'</td>';
+                    echo '<td></td>';
+                    echo '</tr>';
                 ?>
             </tbody>
         </table>
+        </div>
 
         <?php include 'footer.php'; ?>
     </body>
