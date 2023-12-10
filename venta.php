838fe2bd410b895767557b7a881294cb59af9fc5
EazyGuel <83794709+EazyGuel@users.noreply.github.com>
2023-12-10 10:03:33 -0600

    Create venta.php
    Este código da de baja el carrito, y da de baja en existencias del inventario

---
+++ venta.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) > 0) {
+    $carrito = $_SESSION['carrito'];
+    $_SESSION['impuestos']=0;
+    $_SESSION['subtotal'] = 0;
+    $_SESSION['cupon']=0;
+    $_SESSION['descuentototal']=0;
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "sirenegaze";
+    $tabla = "inventario";
+    $total = 0;
+    $precio_final=0;
+    $dcto=0;
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
+    foreach ($carrito as $productoId => $detallesProducto) {
+        if ($detallesProducto['cantidad'] != 0) {
+            // Resta la cantidad del carrito de las existencias actuales en la base de datos
+            $updateQuery = "UPDATE $tabla SET cantidad = cantidad - {$detallesProducto['cantidad']} WHERE Id_producto = $productoId";
+            $conn->query($updateQuery);
+        }
+    }
+    $_SESSION['contador']=0;
+    unset($_SESSION["carrito"]);
+    echo '<script type="text/javascript">
+            var mensaje = "¡Eliminado!";
+            alert(mensaje);
+
+            setTimeout(function() {
+                window.location.href = "index.php";
+            }, 500);
+            </script>';
+}
+?>
