15445c02f8a7a4fba4c907355e8bfb0eec1e11c4
OmarReyesRmz <reyesomar028@gmail.com>
2023-12-10 23:27:17 -0600

    Update total.php

--- total.php
+++ total.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
 
-$totalActualizado = $_SESSION['total'] + $_SESSION['gastosEnvio'] + $_SESSION['impuestos'] - $_SESSION["descuento"];
+// Verifica si $_SESSION['impuestos'] está definido, de lo contrario asigna 0
+$impuestos = isset($_SESSION['impuestos']) ? $_SESSION['impuestos'] : 0;
+
+// Verifica si $_SESSION['descuento'] está definido, de lo contrario asigna 0
+$descuento = isset($_SESSION['descuento']) ? $_SESSION['descuento'] : 0;
+
+// Calcula el $totalActualizado con las variables ajustadas
+$totalActualizado = $_SESSION['total'] + $_SESSION['gastosEnvio'] + $impuestos - $descuento;
+
 
 echo 'T O T A L : $' . number_format($totalActualizado, 0);
 ?>
